3b1840ccec49f9ad670189ae66d1faf122c7156d
Anton Goloma <anton_goloma@ukr.net>
2016-10-20 19:52:13 +0300

    Change output shape to star.

--- TitlesFrame.java
+++ TitlesFrame.java
@@ -18,7 +18,7 @@ public class TitlesFrame extends JFrame
     {
         setTitle("\u041A\u0440\u0438\u0432\u044B\u0435 \u0444\u0438\u0433\u0443\u0440\u044B");
         setDefaultCloseOperation(3);
-        add(new TitlesPanel(78));
+        add(new TitlesPanel(31));
         setSize(350, 350);
         setLocationRelativeTo(null);
     }
